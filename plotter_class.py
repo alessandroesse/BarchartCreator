3ed8608f2aa230a9c1ceedf5e0667df1412af1d0
alessandroesse <siino.alessandro@gmail.com>
2016-03-13 19:50:59 +0100

    added label binder

--- plotter_class.py
+++ plotter_class.py
@@ -24,8 +24,20 @@ class MyBarchartCreator(object):
             for k in data.keys():
                 counter += 1
                 self.data_to_plot[k]= tuple(data[k])
+        self.has_labels = False
 
-    def plot(self):
+    def values_to_label(self, values, is_dict=True, is_list=False):
+        if is_dict == is_list:
+            raise  ParametersError('wrong setting is_dict must be != from is_list')
+        if is_dict:
+            assert isinstance(values, dict)
+            if len(values[0]) != self.N:
+                raise ParametersError('values to label don\'t match with data')
+            self.label_values= values
+            self.has_labels =True
+
+    def plot(self, limit = None):
+
         ind = np.arange(self.N)  # the x locations for the groups
         width = 0.35       # the width of the bars
         fig, ax = plt.subplots()
@@ -35,7 +47,8 @@ class MyBarchartCreator(object):
 
         # add some text for labels, title and axes ticks
         ax.set_ylabel('Time')
-        ax.set_ylim(0, 400)
+        if limit is not None:
+            ax.set_ylim(0, limit)
         ax.set_title('Used time per run to spread the configuration')
         ax.set_xticks(ind + width)
         ax.set_xlabel('# run')
@@ -43,17 +56,20 @@ class MyBarchartCreator(object):
 
         ax.legend((rects1[0], rects2[0]), ('AP stress (throttling 400us)', 'MP stress (throttling 200us/500us)'))
 
-        '''
-        def autolabel(rects):
-            # attach some text labels
-            for rect in rects:
-                height = rect.get_height()
-                ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
-                        '%d' % int(height),
-                        ha='center', va='bottom')
+        if self.has_labels is True:
+
+            def autolabel(rects, values):
+                # attach some text labels
+                counter = 0
+                for rect in rects:
+                    height = rect.get_height()
+                    ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                            '%d' % int(values[counter]),
+                            ha='center', va='bottom')
+                    counter += 1
 
 
-        autolabel(rects1)
-        autolabel(rects2)
-        '''
+            autolabel(rects1,self.label_values[0])
+            autolabel(rects2, self.label_values[1])
+
         plt.show()
--- plotter_from_file.py
+++ plotter_from_file.py
@@ -1,4 +1,3 @@
-import enum
 from column_parser import ParseFile
 from plotter_class import MyBarchartCreator
 
@@ -10,5 +9,9 @@ n_attempt =list()
 for i in range(0, len(columns[0])):
     n_attempt.append((i+1))
 
+vals=dict()
+vals[0]= [1,2,3,4,5,6,7,8,9,10]
+vals[1]=[100,2,3,4,5,6,7,8,9,10]
 mb= MyBarchartCreator(columns)
-mb.plot()
+mb.values_to_label(vals)
+mb.plot(limit=550)
